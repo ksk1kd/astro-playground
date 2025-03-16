6b5dc7fb09c94ba92420ea8fc1cbf76f924ded47
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-03-17 02:07:50 +0900

    add prefetch config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: {
+    defaultStrategy: 'viewport',
+    prefetchAll: true
+  },
   adapter: node({
     mode: 'standalone'
   })
